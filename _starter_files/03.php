2f5a449ae64d20a72e6bd20f209ac035788a35e4
PhilipArlandAlili <202080139@psu.palawan.edu.ph>
2023-08-10 18:55:40 +0800

    added file

---
+++ _starter_files/03.php
@@ -0,0 +1,14 @@
+<!-- 
+    Write a program that takes a string as input and outputs the frequency of each character in the string.
+-->
+
+<?php
+    Function output($string) {
+
+        if (strlen($string))
+
+
+
+    }
+
+?>
