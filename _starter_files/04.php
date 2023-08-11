aa8c8d754bfd9f0a0ef864dd199b89dddb7745e9
PhilipArlandAlili <202080139@psu.palawan.edu.ph>
2023-08-12 00:00:43 +0800

    add php

--- _starter_files/04.php
+++ _starter_files/04.php
@@ -1,4 +1,19 @@
 <!-- 
     Write a program that takes a number as input and outputs the sum of its digits.
- -->
+    numa
+    numb
+    sum
+-->
 
+
+<?php
+
+function number($numbs) {
+    numA = 3;
+    numB = 7;
+
+    
+}
+
+
+?>
