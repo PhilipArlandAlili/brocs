691b0625e3e39ad791e8e45f19c46476d7939e1a
PhilipArlandAlili <202080139@psu.palawan.edu.ph>
2023-08-12 03:53:26 +0800

    add php

---
+++ Strings/var_dump.php
@@ -0,0 +1,22 @@
+
+
+<?php
+
+$a = 32;
+echo var_dump($a) . "<br>";
+
+$b = 32.2;
+echo var_dump($b) . "<br>";
+
+$c = "Hello motherfucker"; 
+echo var_dump($c) ."<br>";
+
+$d = array("red", "green", "blue");
+echo var_dump($d) . "<br>";
+
+$e = [1, 2, 3, 4];
+echo var_dump($e) . "<br>";
+
+echo var_dump($a, $b, $c);
+
+?>
